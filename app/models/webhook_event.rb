ee8eb05bcec9282e8437b77189ba444ed2d6a2ce
Perry Hertler <perry.hertler@gusto.com>
2022-12-19 13:00:29 -0600

    broadcast events

--- app/models/webhook_event.rb
+++ app/models/webhook_event.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class WebhookEvent < ApplicationRecord
+  after_create_commit :stream
+
   belongs_to :subscriber
+
+  private
+
+  def stream
+    broadcast_prepend_later_to(subscriber, :webhook_events, target: :webhook_events)
+  end
 end
