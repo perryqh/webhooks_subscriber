341af36d59dbb4d229913e58fa2b5b75a63a0578
Perry Hertler <perry.hertler@gusto.com>
2022-12-19 16:44:08 -0600

    formatting json

--- app/javascript/controllers/beautifyjson_controller.js
+++ app/javascript/controllers/beautifyjson_controller.js
@@ -3,8 +3,16 @@ import Beautify from "json-format-highlight"
 export default class extends Controller {
     static targets = [ "payloadJson" ]
     connect() {
-        var beauty = Beautify(this.payloadJsonTarget.dataset.actualJson)
-        console.log(beauty)
+        const customColorOptions = {
+            keyColor: 'black',
+            numberColor: 'blue',
+            stringColor: '#0B7500',
+            trueColor: '#00cc00',
+            falseColor: '#ff8080',
+            nullColor: 'cornflowerblue'
+        };
+        let json = this.payloadJsonTarget.dataset.actualJson
+        var beauty = Beautify(json, customColorOptions)
         this.payloadJsonTarget.innerHTML = beauty
     }
 }
