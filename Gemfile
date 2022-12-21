586e64a30f32c00e0b0bebd18b59a3654cd0243e
Perry Hertler <perry.hertler@gusto.com>
2022-12-20 22:10:16 -0600

    specing broadcast

--- Gemfile
+++ Gemfile
@@ -41,8 +41,8 @@ gem 'bootsnap', require: false
 
 # Use Sass to process CSS
 gem 'bootstrap'
-gem 'sassc-rails'
 gem 'local_time'
+gem 'sassc-rails'
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- spec/models/webhook_event_spec.rb
+++ spec/models/webhook_event_spec.rb
@@ -1,5 +1,34 @@
 # frozen_string_literal: true
 
 RSpec.describe WebhookEvent, type: :model do
+  subject(:webhook_event) { build(:webhook_event) }
+
+  let(:subscriber) { webhook_event.subscriber }
+
   it { is_expected.to belong_to :subscriber }
+
+  describe 'turbo broadcast' do
+    context 'when creating' do
+      it 'broadcasts event' do
+        allow(webhook_event).to receive(:broadcast_prepend_later_to)
+        webhook_event.save!
+        expect(webhook_event)
+          .to have_received(:broadcast_prepend_later_to)
+          .with(subscriber,
+                :webhook_events,
+                target: :webhook_events,
+                locals: { highlight: true })
+      end
+    end
+
+    context 'when updating' do
+      before { webhook_event.save! }
+
+      it 'does not broadcast event' do
+        allow(webhook_event).to receive(:broadcast_prepend_later_to)
+        webhook_event.update!(payload: { name: 'event-name' })
+        expect(webhook_event).not_to have_received(:broadcast_prepend_later_to)
+      end
+    end
+  end
 end
